30b1958bdea8970fd0eb17a49f39bf2386e15718
Maicon Botelho <maicon@maiconbotelho.com.br>
2024-03-09 20:08:16 -0300

    Criação de calculadora

--- calculadora.py
+++ calculadora.py
@@ -6,31 +6,33 @@ print('- subtração')
 print('* Multiplicação')
 print('/ Divisão')
 
-
-operador = input('\nQual operação você quer realizar? ')
-
-if (operador == '+') or (operador == '-') or (operador == '*') or (operador == '/'):
-    n1 = int(input('Digite o primeiro número: '))
-    n2 = int(input('Digite o segundo número: '))
+while True:
+    operador = input('\nQual operação você quer realizar? ')
+    if (operador == '+') or (operador == '-') or (operador == '*') or (operador == '/'):
+        n1 = int(input('Digite o primeiro número: '))
+        n2 = int(input('Digite o segundo número: '))
 
     if (operador == '+'):
         resultado = n1 + n2
-        print('\nResultado {} {} {} = {}'.format(n1, operador, n2, resultado))
-
+        print('Resultado {} {} {} = {}'.format(n1, operador, n2, resultado))
+        continue
     elif (operador == '-'):
         resultado = n1 - n2
-        print('\nResultado {} {} {} = {}'.format(n1, operador, n2, resultado))
-
+        print('Resultado {} {} {} = {}'.format(n1, operador, n2, resultado))
+        continue
     elif (operador == '*'):
         resultado = n1 * n2
-        print('\nResultado {} {} {} = {}'.format(n1, operador, n2, resultado))
-
+        print('Resultado {} {} {} = {}'.format(n1, operador, n2, resultado))
+        continue
     elif (operador == '/'):
         resultado = n1 / n2
-        print('\nResultado {} {} {} = {}'.format(n1, operador, n2, resultado))
+        print('Resultado {} {} {} = {}'.format(n1, operador, n2, resultado))
+        continue
+    elif (operador == 's'):
+        break
 
-else:
-    print('Operação invalida!!!')
+    else:
+        print('Operação invalida!!!')
 
 print('Encerrando o programa...')
 
